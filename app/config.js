95ed45874c1815343ac9e299d2d103f68dabfab5
Dave Mercer <davemercer77@gmail.com>
2022-02-22 16:21:20 +0000

    Service name change

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
     // Service name used in header. Eg: 'Renew your passport'
-    serviceName: 'Design and build digital services for Defence',
+    serviceName: 'Defence Service Manual',
 
     // Default port that prototype runs on
     port: '3000',
